57f9b72da971e1075f829855918863f0961eb8cc
teju-doyin <tj.doyin@gmail.com>
2024-06-18 20:28:59 +0100

    changed popup height and moved error message

--- components/waitlist/WaitlistModal.jsx
+++ components/waitlist/WaitlistModal.jsx
@@ -98,17 +98,17 @@ const EmailWaitlistModal = ({ isOpen, onRequestClose }) => {
   return (
     <div className={`fixed inset-0 flex items-center justify-center z-[1002] transition-opacity duration-500 ${isOpen ? 'opacity-100' : 'opacity-0'}`}>
       <div className={`fixed inset-0  bg-[#000000CC] bg-opacity-80 transition-opacity duration-500 ${isOpen ? 'opacity-75' : 'opacity-0'}`}></div>
-      <div className={`bg-[#F4FF1] relative h-[100dvh] shadow-lg  w-[75%]  md:w-[80%]  lg:max-w-[60rem] transform transition-transform duration-500 ${isOpen ? 'scale-100 opacity-100' : 'scale-95 opacity-0'}`}>
+      <div className={`bg-[#F4FF1] relative h-[90dvh] shadow-lg  w-[75%]  md:w-[80%]  lg:max-w-[60rem] transform transition-transform duration-500 ${isOpen ? 'scale-100 opacity-100' : 'scale-95 opacity-0'}`}>
         <Image src={close} onClick={handleClose} className='absolute right-0 cursor-pointer ml-auto '  alt=''/>
         <div className="flex flex-col  items-center  lg:w-[80%] lg:mx-auto">
             <Image src={qrImage} alt='' className=' w-full basis-[50%]'/>
             <div className="bg-white h-[30rem] basis-[70%] py-5 min-h-[18rem]">
               <div className="w-[90%] mx-auto">
                 <h3 className="text-black  mb-4 text-[30px] font-semibold ">Join the Waitlist</h3>
-                <p className='text-[.9rem] '>Get an head start for you restaurants & hotels with our Scan-To-Order QR Code system</p>
+                <p className='text-[.9rem] mb-4'>Get an head start for you restaurants & hotels with our Scan-To-Order QR Code system</p>
                 <div className=" flex flex-col-reverse md:flex-col  ">
                   {submitted? <p className='text-[#27BD90] font-medium'>Thank you for joining the waitlist!</p>:
-                      <form onSubmit={sendEmail}  className="flex h-[50px] flex-col md:flex-row gap-4 md:gap-0 ">
+                      <form onSubmit={sendEmail}  className="flex h-[50px] flex-col  gap- ">
                           <input
                           type="email"
                           value={email}
@@ -116,6 +116,9 @@ const EmailWaitlistModal = ({ isOpen, onRequestClose }) => {
                           placeholder='enter your email here'
                           className=" border-b border-b-[#00000040] pb-2 text-[#00000040] font-semibold text-[.8rem] outline-none md:basis-[80%] "
                           />
+                          <div className="min-h-[24px]">
+                            {message && <p className={`text-[14px] font-semibold ${setMessageType==='error'|| !email ?'text-[#F21010] ':'text-[#27BD90]'} `}>{message}</p>}
+                          </div>
                           <button type="submit" disabled={loading}  className=" bg-[#171717] text-[.8rem] text-white min-h-12">
                           {loading ? (
                               <ClipLoader
@@ -129,9 +132,7 @@ const EmailWaitlistModal = ({ isOpen, onRequestClose }) => {
                           </button>
                       </form>
                   }
-                  <div className="min-h-[24px]">
-                    {message && <p className={`mt-4 text-[14px] font-semibold ${setMessageType==='error'|| !email ?'text-[#F21010] ':'text-[#27BD90]'} `}>{message}</p>}
-                  </div>
+                  
                 </div>
               </div>
             </div>
